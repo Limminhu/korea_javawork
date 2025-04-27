e186df8575121a2f5bc17147703230f26237d16f
minhu1117 <minhu1117@naver.com>
2025-04-27 15:24:47 +0900

    banking, exception

---
+++ Chapter4/src/classes/bookTest2.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class bookTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//객체 배열 생성
+//		Book[] books = new Book[3];
+//		for(int i = 0;i < books.length;i++) {
+//			System.out.println(books[i]);
+//		}
+//		//원소 추가(저장)
+//		books[0] = new Book(100, "채식주의자","한강");
+//		books[1] = new Book(101, "자바프로그래밍 입문","박은종");
+//		books[2] = new Book(102, "모두의 파이썬","이승찬");
+		//객체 배열 생성 방법 2
+		Book[] books = {new Book(100,"채식주의자","한강"),
+				new Book(101,"자바프로그래밍 입문","박은종"),new Book(102,"모두의 파이썬","이승찬")};
+		//원소 출력
+		for(int i = 0;i < books.length;i++) {
+			books[i].showInfo();
+		}
+
+	}
+
+}
---
+++ Chapter7/src/chpter7/ToString.java
@@ -0,0 +1,30 @@
+package chpter7;
+class MyDog{
+	String name;String type;
+	
+	public MyDog(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return type + " " + name;
+	}
+	
+//	public String showInfo() {
+//		return type + " " + name;
+//	}
+}
+public class ToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MyDog dog = new MyDog("백구", "진돗개");
+		System.out.println(dog);
+//		System.out.println(dog.showInfo());
+	}
+
+}
